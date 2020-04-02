fbed2cb1cbaced6340670e57bbbbce9df084bfbd
ae3e <dev@ae3e.com>
2020-04-02 14:20:53 +0200

    Improved UI

--- src/SimpleEditor.tsx
+++ src/SimpleEditor.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
-import { PanelOptionsGroup, FormLabel } from '@grafana/ui';
+import { PanelOptionsGroup } from '@grafana/ui';
 import { PanelEditorProps } from '@grafana/data';
-import {config} from '@grafana/runtime'
+import { config } from '@grafana/runtime'
 
 import { SimpleOptions } from './types';
 
@@ -50,63 +50,80 @@ export class SimpleEditor extends PureComponent<PanelEditorProps<SimpleOptions>>
     });
   };
 
-  onTypeChanged = (evt: any) => {};
+  onTypeChanged = (evt: any) => { };
 
   render() {
-    let theme = config.theme.isDark?"tomorrow_night":"tomorrow";
+    let theme = config.theme.isDark ? "tomorrow_night" : "tomorrow";
 
     return (
-      <PanelOptionsGroup title="My Panel Options">
+
+      <PanelOptionsGroup title="Options">
         {/*<FormField label="Title" value={this.props.options.title} onChange={this.onTextChanged} />*/}
 
-        <div className="form-field">
-          <FormLabel>{'Data'}</FormLabel>
-          <AceEditor
-            mode="javascript"
-            theme={theme}
-            name="dashboard_data"
-            height="150px"
-            value={JSON.stringify(this.props.options.data, null, 4)}
-            onBlur={this.onDataChanged}
-          />
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Data</h5>
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <AceEditor
+                mode="javascript"
+                theme={theme}
+                name="dashboard_data"
+                height="150px"
+                value={JSON.stringify(this.props.options.data, null, 4)}
+                onBlur={this.onDataChanged}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Layout</h5>
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <AceEditor
+                mode="javascript"
+                theme={theme}
+                name="dashboard_layout"
+                height="150px"
+                width="350px"
+                value={JSON.stringify(this.props.options.layout, null, 4)}
+                onBlur={this.onLayoutChanged}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Config</h5>
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <AceEditor
+                mode="javascript"
+                theme={theme}
+                name="dashboard_config"
+                height="150px"
+                width="300px"
+                value={JSON.stringify(this.props.options.config, null, 4)}
+                onBlur={this.onConfigChanged}
+              />
+            </div>
+          </div>
+        </div>
 
-<FormLabel>{'Layout'}</FormLabel>
-          <AceEditor
-            mode="javascript"
-            theme={theme}
-            name="dashboard_layout"
-            height="150px"
-            value={JSON.stringify(this.props.options.layout, null, 4)}
-            onBlur={this.onLayoutChanged}
-          />
-        </div>
         <br />
-        <div className="form-field">
-          
-        </div>
-        <br />
-        <div className="form-field">
-          <FormLabel>{'Config'}</FormLabel>
-          <AceEditor
-            mode="javascript"
-            theme={theme}
-            name="dashboard_config"
-            height="150px"
-            value={JSON.stringify(this.props.options.config, null, 4)}
-            onBlur={this.onConfigChanged}
-          />
-        </div>
-        <br />
-        <div className="form-field">
-          <FormLabel>{'Script'}</FormLabel>
-          <AceEditor
+        <div className="section gf-form-group"  style={{ display: 'block', width: '100%' }}>
+          <h5 className="section-heading">Script</h5>
+          <div className="gf-form-inline"  style={{ display: 'block', width: '100%' }}>
+            <div className="gf-form">
+            <AceEditor
             mode="javascript"
             theme={theme}
             name="dashboard_script"
             height="150px"
+            width="100%"
             value={this.props.options.script}
             onBlur={this.onScriptChanged}
           />
+            </div>
+          </div>
         </div>
       </PanelOptionsGroup>
     );
