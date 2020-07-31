137de6ef024d9cc17e7a4104a9d2dd750775d85d
ae3e <dev@ae3e.com>
2020-07-31 09:10:03 +0200

    Add default values

--- src/types.ts
+++ src/types.ts
@@ -9,15 +9,34 @@ export interface SimpleOptions {
 
 export const defaults: SimpleOptions = {
   title: 'title',
-  layout: {},
-  config:{},
+  layout: {
+    "font": {
+        "color": "darkgrey"
+    },
+    "paper_bgcolor": "rgba(0,0,0,0)",
+    "plot_bgcolor": "rgba(0,0,0,0)",
+    "xaxis": {
+        "type": "date"
+    }
+  },
+  config:{
+    "displayModeBar": false
+  },
   data: [
     {
       type: 'scatter',
-      mode: 'lines+points',
-      marker: { color: 'red' },
+      mode: 'lines',
+      line: { color: 'red', with:2 },
     },
   ],
-  script: '',
-  onclick:''
+  script: `console.log(data)
+var trace = {
+  x: data.series[0].fields[0].values.buffer,
+  y: data.series[0].fields[1].values.buffer
+};
+  
+return {data:[trace],layout:{title:'My Chart'}};`,
+  onclick:`console.log(data)
+//Not working if variable is a Textbox : Maximum call stack size exceeded. Grafana bug?
+window.updateVariables({query:{'var-project':'toti'}, partial: true})`
 };
